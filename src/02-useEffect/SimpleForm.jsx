bb70f4836bb08cd55597c4903288c5ffc3f5ef09
Rodox <rodrigo17.ed@gmail.com>
2023-12-27 21:54:51 -0600

    Added SimpleForm component with useState and useEffect hooks

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'user',
+        email: 'username@gmail.com'
+    });
+
+    const {username, email} = formState;
+
+    const onInputChange = ({target}) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+
+    useEffect( () => {
+        console.log('useEffect Called')
+    }, [ ]);
+
+    useEffect( () => {
+        console.log('formState changed')
+    }, [formState]);
+  
+    useEffect( () => {
+        console.log('email change')
+    }, [email]);
+
+    
+
+    return (
+        <>  
+            <h1>Formulario Simple</h1>
+            
+            <hr />
+
+            <input 
+                type="text" 
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type="email" 
+                className="form-control mt-2"
+                placeholder="user@gmail.com"
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+            />
+        </>    
+    );
+};
