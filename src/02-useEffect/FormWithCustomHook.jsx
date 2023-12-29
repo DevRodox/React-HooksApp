e523b0dee1c62003976bd4b874c83e285e4cd748
Rodox <rodrigo17.ed@gmail.com>
2023-12-28 20:15:14 -0600

    reset button added

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -3,7 +3,7 @@ import { useForm } from "../hooks/useForm";
 
 export const FormWithCustomHook = () => {
 
-    const {formState, onInputChange, username, email, password} = useForm(
+    const { onInputChange, onResetForm, formState, username, email, password} = useForm(
         {
             username: '',
             email: '',
@@ -45,6 +45,8 @@ export const FormWithCustomHook = () => {
                 value={ password }
                 onChange={ onInputChange }
             />
+
+            <button onClick={ onResetForm } className="btn btn-primary mt-2" >Borrar</button>
         </>    
     );
 };
