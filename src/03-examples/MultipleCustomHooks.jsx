991a7c39c457bac6282f274b793e6f272de7cfd0
Rodox <rodrigo17.ed@gmail.com>
2023-12-28 21:38:16 -0600

    The API data was displayed, and loading messages were added

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,16 +1,39 @@
 import { useFetch } from '../hooks/useFetch';
+import { useCounter } from '../hooks/useCounter';
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/1');
+    const {counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://rickandmortyapi.com/api/character/${counter}`);
+    const { name, species } = data || {};
 
-    console.log({ data, isLoading, hasError });
+    console.log(data);
 
     return (
         <>
-            <h1> Breaking bad Quotes </h1>
+            <h1> Rick & Morty Characters </h1>
             <hr />
-            
+
+            {
+                isLoading ? ( 
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>
+                ):
+                (
+                    <blockquote className='blockquote text-end'>
+                    <p className='mb-1'>{ name }</p>
+                    <footer className='blockquote-footer'>{ species }</footer>
+                    </blockquote>
+                )
+            }
+
+            <button 
+                className='btn btn-primary' 
+                onClick={ () => increment() }
+                disabled={ isLoading }>
+                Next Character
+            </button>
         </>
     );
 };
