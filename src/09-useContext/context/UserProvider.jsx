63ff7de5bdd0338a47eadcea607e454465507fd6
Rodox <rodrigo17.ed@gmail.com>
2024-01-11 23:45:42 -0600

    useContext example was added

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,12 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-    id: 123,
-    name: 'Rodrigo',
-    mail: 'rodrigo@google.com' 
-};
-
 export const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState();
+
     return(
-        <UserContext.Provider value={{ hola: 'Mundo', user }}>
+        <UserContext.Provider value={{ user, setUser}}>
             { children }
         </UserContext.Provider>
     );
