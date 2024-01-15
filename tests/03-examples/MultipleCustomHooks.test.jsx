206c068742c79ac9f00561d0a9874cb05589647b
Rodox <rodrigo17.ed@gmail.com>
2024-01-15 00:13:28 -0600

    MultipleCustomHooks tests were added

--- tests/03-examples/MultipleCustomHooks.test.jsx
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,10 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import { MultipleCustomHooks } from "../../src/03-examples";
-import { useFetch } from "../../src/hooks/useFetch";
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MultipleCustomHooks } from '../../src/03-examples';
+import { useFetch } from '../../src/hooks/useFetch';
+import { useCounter } from '../../src/hooks/useCounter';
 
 jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
 
 describe('Pruebas en <MultipleCustomHooks.test />', () => {
+
+    const mockIncrement = jest.fn();
+
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    });
+
+    beforeEach( () => {
+        jest.clearAllMocks();
+    });
 
     test('debe de mostrar el componente por defecto', () => {
 
@@ -36,5 +49,22 @@ describe('Pruebas en <MultipleCustomHooks.test />', () => {
         expect( screen.getByText('human') ).toBeTruthy();
         expect( screen.getByText('morty') ).toBeTruthy();
         expect( nextButton.disabled ).toBeFalsy();
+    });
+
+    
+    test('debe de llamar la funcion de incrementar', () => {
+
+        useFetch.mockReturnValue({
+            data: { name: 'morty', species: 'human', image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Frickandmorty.fandom.com%2Fwiki%2FMorty_Smith&psig=AOvVaw1Soy5OTc0j1cstPyt8XKVQ&ust=1705383332892000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNj7kZHW3oMDFQAAAAAdAAAAABAD'},
+            isLoading: false,
+            hasError: null
+        });
+
+        render( <MultipleCustomHooks /> );
+        const nextButton = screen.getByRole('button');
+
+        fireEvent.click( nextButton );
+        expect( mockIncrement ).toHaveBeenCalled();
+        
     });
 });
