b1daba7ca462936e97fd940144eb14e02c688eca
Rodox <rodrigo17.ed@gmail.com>
2024-01-14 23:21:27 -0600

    MultipleCustomHooks tests were added

---
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { MultipleCustomHooks } from "../../src/03-examples";
+
+describe('Pruebas en <MultipleCustomHooks.test />', () => {
+
+    test('debe de mostrar el componente por defecto', () => {
+
+        render( <MultipleCustomHooks /> );
+        expect( screen.getByText('Loading...') ); 
+        expect( screen.getByText('Rick & Morty Characters') );
+        
+        expect()
+    });
+});
